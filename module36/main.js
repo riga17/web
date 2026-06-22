2570bd112b8f9b916510e53f6c56b02bb6524203
riga17 <rigahyseni17@gmail.com>
2026-06-22 19:08:12 +0200

    module36

---
+++ module36/main.js
@@ -0,0 +1,11 @@
+$(window).ready(function(){
+    alert('it is working');
+
+$('#test1').css('color','yellow');
+$('.test2').css('background-color','darkblue');
+$('#myform :input').css('border','3px solid red');
+$('div :visible').css('color','yellow');
+});
+
+
+
